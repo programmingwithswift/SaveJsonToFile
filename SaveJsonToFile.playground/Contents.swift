0324c6d442ccf1bc00882152508df559320fde61
Darren <darren@MacBook-Pro.lan>
2020-08-24 18:34:13 +0200

    initial commit

---
+++ SaveJsonToFile.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+// Writing string to file
+let jsonString = "{\"location\": \"the moon\"}"
+
+if let documentDirectory = FileManager.default.urls(for: .documentDirectory,
+                                                    in: .userDomainMask).first {
+    let pathWithFilename = documentDirectory.appendingPathComponent("myJsonString.json")
+    do {
+        try jsonString.write(to: pathWithFilename,
+                             atomically: true,
+                             encoding: .utf8)
+    } catch {
+        // Handle error
+    }
+}
+
+// Writing data to file
+if let jsonData = jsonString.data(using: .utf8),
+    let documentDirectory = FileManager.default.urls(for: .documentDirectory,
+                                                     in: .userDomainMask).first {
+    let pathWithFileName = documentDirectory.appendingPathComponent("myJsonData")
+    do {
+        try jsonData.write(to: pathWithFileName)
+    } catch {
+        // handle error
+    }
+}
